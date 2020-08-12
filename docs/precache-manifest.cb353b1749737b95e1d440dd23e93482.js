be9e8d6ba0ea651d231567c1c2c7877498b63365
juan galarce <j6alarce@gmail.com>
2020-08-11 22:58:41 -0400

    docs agregado

---
+++ docs/precache-manifest.cb353b1749737b95e1d440dd23e93482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6519398a3c6f882c1ccad5e4a8088904",
+    "url": "/index.html"
+  },
+  {
+    "revision": "165c861e50ba4dcf66c3",
+    "url": "/static/css/main.cda70b8a.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "165c861e50ba4dcf66c3",
+    "url": "/static/js/main.2cff32cd.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
